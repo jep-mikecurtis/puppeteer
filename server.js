038d47e73e054010b1ebece320923ac4751f7a00
jep-mikecurtis <mcurtis@jepcapital.com>
2019-09-27 07:43:03 -0700

    git test

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 const writeStream = fs.createWriteStream('post.csv');
 require('dotenv').config();
+// Git change
 
 // Write Headers
 writeStream.write(`Status,ID,Name,Business Type,City,Phone,Expiration\n`);
@@ -13,7 +14,10 @@ app.get('/', async (req, res) => {
     const url = 'https://dmv2u.oregon.gov/eServices/_/';
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.setViewport({ width: 1440, height: 1080 });
+    await page.setViewport({
+      width: 1440,
+      height: 1080
+    });
     await page.goto(url);
     await page.click('#l_p-1-1');
     await page.waitForSelector('#c-7');
@@ -21,26 +25,48 @@ app.get('/', async (req, res) => {
     await page.waitForSelector('#c-a');
     await page.click('#c-a');
     await page.waitFor(3000);
-    await page.screenshot({ path: 'whereAmI.png' });
-
-    // Build Array
-    const table = await page.evaluate(() => {
-      const tds = Array.from(document.querySelectorAll('.DTC span'));
-      return tds.map(td => td.innerHTML);
+    await page.screenshot({
+      path: 'whereAmI.png'
     });
 
-    await browser.close();
-    for (let i = 0; i < table.length; i += 7) {
-      writeStream.write(
-        `${table[i]},${table[i + 1]},${table[i + 2]},${table[i + 3]},${
-          table[i + 4]
-        },${table[i + 5]},${table[i + 7]}\n`
-      );
-    }
+    const businesses = [];
+    let moreResults = true;
 
-    res.json({ success: true });
+    while (moreResults) {
+      const record = await page.evaluate(() => {
+        const rowNodeList = document.querySelectorAll('.DocTableBody tr');
+        const rowArray = Array.from(rowNodeList);
+        return rowArray.slice(1).map(tr => {
+          const dataNodeList = tr.querySelectorAll('td');
+          const dataArray = Array.from(dataNodeList);
+
+          return dataArray.map(td => {
+            return td.textContent
+              .replace(/Status: /g, '')
+              .replace(/ID: /g, '')
+              .replace(/Name: /g, '')
+              .replace(/Business Type: /g, '')
+              .replace(/Business City: /g, '')
+              .replace(/Phone Number: /g, '')
+              .replace(/Expiration: /g, '');
+          });
+        });
+      });
+
+      businesses.push(...record);
+
+      try {
+        await page.click('#c-m_pgnext');
+      } catch (error) {
+        moreResults = false;
+      }
+    }
+    await browser.close();
+    res.json(businesses);
   } catch (err) {
-    res.json({ err });
+    res.json({
+      err
+    });
   }
 });
 
